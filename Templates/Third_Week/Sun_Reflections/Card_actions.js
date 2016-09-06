2256408e0dced7dca97dc456a505371af9816b51
FlaviuCernat <flaviucernat@gmail.com>
2016-09-06 20:44:03 +0300

    Minor modifications
    Rearranging the files

--- Templates/Third_Week/Sun_Reflections/Card_actions.js
+++ Templates/Third_Week/Sun_Reflections/Card_actions.js
@@ -10,6 +10,7 @@ function mouseOver() {
     document.getElementById('m_letter').style.border = "2px solid gray";
     document.getElementById('e_letter').style.border = "2px solid gray";
 }
+
 function mouseOut() {
     var x = document.getElementsByClassName("left");
     var i;
@@ -27,13 +28,33 @@ function open () {
   document.getElementById('activated_footer').style.height = "100%";
   document.getElementById('activated_footer').style.paddingTop = "2em";
   document.getElementById('activated_footer').style.paddingBottom= "92px";
-  document.getElementById('java_sun').style.border = "2px solid rgb(238, 238, 238)";
+  document.getElementById('java_sun').style.border = "2px solid rgb(233, 233, 233)";
   document.getElementById('java_sun').style.borderRadius = "7px";
   document.getElementById('java_sun').style.transition = ".3s";
 }
+
 function close () {
   document.getElementById('activated_footer').style.height = "10px";
   document.getElementById('activated_footer').style.paddingTop = "0";
   document.getElementById('activated_footer').style.paddingBottom= "0";
   document.getElementById('java_sun').style.border = "2px solid white";
 }
+
+(function dateTime() {
+  var d = new Date();
+  var h = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('date').innerHTML = d.toDateString();
+  document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(dateTime, 500);
+  function checkTime(i) {
+    if (i < 10) {
+      // add zero in front of numbers < 10
+      i = "0" + i;
+    }
+  return i;
+  }
+}());
--- Tetris/app/js/tetris.js
+++ Tetris/app/js/tetris.js
@@ -2,7 +2,9 @@
 var row = 0, col = 5, i;
 var color = ["blue", "red", "rgb(0, 255, 0)", "yellow", "orange", "rgb(204, 0, 153)", "#00e4e4"];
 var colorIndex = getRandomInt(0, 6);
-var time = 100;
+var time = 200;
+var gameOver
+
 function draw(row, col) {
   return document.getElementById("row-" + row + "-col-" + col);
 }
@@ -12,6 +14,7 @@ function getRandomInt(min, max) {
 }
 
 function play() {
+  clearInterval(gameOver);
   var tableGame = document.getElementById("table_game");
   var tableData = tableGame.getElementsByTagName("TD");
   var i;
@@ -25,7 +28,7 @@ function play() {
 
 function tetrisGame() {
 
-  var gameOver = setInterval(gameLogic, time);
+  gameOver = setInterval(gameLogic, time);
 
   function gameLogic() {
 
@@ -33,7 +36,9 @@ function tetrisGame() {
 
     blockOnTop(firstRowFull);
 
-    if (firstRowFull[0] && row === 0) {  //daca primul si ultimul rand sunt ocupate => game over
+    //daca primul si ultimul rand sunt ocupate => game over
+
+    if (firstRowFull[0] && row === 0) {
       clearInterval(gameOver);
       alert("GAME OVER");
 
@@ -54,12 +59,17 @@ function tetrisGame() {
 
 function blockOnTop(firstRowFull) {
 
-  for (i = 1; i <= 10; i++) { // creez un array sa stiu daca vreun element de pe primul rand e ocupat
-    firstRowFull.push(document.getElementById("row-1-col-"+i).hasAttribute("style"));
+  // creez un array sa stiu daca vreun element de pe primul rand e ocupat
+
+  for (i = 1; i <= 10; i++) {
+    firstRowFull.push(draw(1, i).hasAttribute("style"));
   }
 
   for (i = 0; i < 10; i++) {
-    if (firstRowFull[i] === true) //daca am vreun element de pe primul rand ocupat, il pun primul
+
+    //daca am vreun element de pe primul rand ocupat, il pun primul
+
+    if (firstRowFull[i] === true)
       firstRowFull[0] = firstRowFull[i];
   }
 }
@@ -69,65 +79,49 @@ function squareMovement() {
   window.onkeydown = function(e) {
 
     if (row < 15) {
-      var haveBlockDown = document.getElementById("row-" + (row + 1) + "-col-" + col).hasAttribute("style");
+      var haveBlockDown = draw(row + 1, col).hasAttribute("style");
     }
 
     if ((col > 1  && row > 0)) {
-      var haveBlockLeft = document.getElementById("row-" + row + "-col-" + (col - 1)).hasAttribute("style");
+      var haveBlockLeft = draw(row, col - 1).hasAttribute("style");
     }
 
     if (col < 10 && row > 0) {
-      var haveBlockRight = document.getElementById("row-" + row + "-col-" + (col + 1)).hasAttribute("style");
+      var haveBlockRight = draw(row, col + 1).hasAttribute("style");
     }
 
     if (e.keyCode === 37 && col > 1  && haveBlockLeft === false) {
       col --;
       draw(row, col).style.backgroundColor = color[colorIndex];
-      draw(row + 1, col).style.backgroundColor = color[colorIndex];
-      draw(row + 1, col+1).style.backgroundColor = color[colorIndex];
-      draw(row, col +2).style = null;
-      draw(row + 1, col + 2).style = null;
-      // for (i = 1; i < 15; i++) {
-      //   for (j = 1; j < 10; j++) {
-      //     if(draw(i, j).hasAttribute("style")) {
-      //       draw(i, j-1).style.backgroundColor = color[colorIndex];
-      //       draw(i, j).style = null;
-      //     }
-      //   }
-      // }
+      draw(row, col + 1).style = null;
     }
 
     if (e.keyCode === 39 && col < 10 && haveBlockRight === false) {
       col++;
-      document.getElementById("row-" + row + "-col-" + col).style.backgroundColor = color[colorIndex];
-      document.getElementById("row-" + row + "-col-" + (col - 1)).style = null;
+      draw(row, col).style.backgroundColor = color[colorIndex];
+      draw(row, col - 1).style = null;
     }
 
     if (e.keyCode === 40 && row < 15 && haveBlockDown === false) {
       row++;
-      document.getElementById("row-" + row + "-col-" + col).style.backgroundColor = color[colorIndex];
+      draw(row, col).style.backgroundColor = color[colorIndex];
       if ((row - 1) > 0)
-        document.getElementById("row-" + (row - 1) + "-col-" + col).style = null;
+        draw(row - 1, col).style = null;
     }
   };
  }
 
  function NextBlockNotOccupied() {
   if (row < 15) {
-    var nextBlock = document.getElementById("row-" + (row + 1) + "-col-" + col).hasAttribute("style");
+    var nextBlock = draw(row + 1, col).hasAttribute("style");
   }
 
-  if (row < 14) {
-    var secondNextBlock = document.getElementById("row-" + (row + 2) + "-col-" + col).hasAttribute("style");
-  }
-
-   if (nextBlock && secondNextBlock) {
+   if (nextBlock) {
      row = 16;
    } else {
      row++;
    }
    return nextBlock;
-   return secondNextBlock;
  }
 
  function blockDownNewSquare()  {
@@ -139,17 +133,13 @@ function squareMovement() {
  }
 
  function blockMovingDown()  {
-     if (row < 15) {
-       square();
+     if (row < 16) {
+       var currentBlock = draw(row, col);
+       currentBlock.style.backgroundColor = color[colorIndex];
        if ((row - 1) > 0) {
-         var deleteBlock = draw(row-1, col);
+         var deleteBlock = draw(row - 1, col);
          deleteBlock.style = null;
-         draw(row-1, col+1).style = null;
        }
-
-      //  var createBlock = document.getElementById("row-"+ row + "-col-" + col);
-      //  createBlock.style.backgroundColor = color[colorIndex];
-
      }
    }
 
@@ -158,23 +148,27 @@ function clearFullRow() {
 
  var bottomRow = [];
  for (j = 1; j <= 10; j++) {
-  bottomRow.push(document.getElementById("row-15-col-"+j).hasAttribute("style"));
+  bottomRow.push(draw(15, j).hasAttribute("style"));
  }
- bottomRow.sort(); //imi ordonez valorile de true si false, iar daca primul e true
-                  //  inseamna ca toate sunt true, deci randul e plin
+ bottomRow.sort();
+
+  //imi ordonez valorile de true si false, iar daca primul e true
+  //  inseamna ca toate sunt true, deci randul e plin
+
  if (bottomRow[0])
    for (var jj = 1; jj <= 10; jj++) {
-     document.getElementById("row-15" + "-col-" + jj).style = null;
+     draw(15, jj).style = null;
    }
 
  if (bottomRow[0]) {
    for (i = 15; i > 0; i--)
      for (j = 1; j < 10; j++) {
-       var currentBlock = document.getElementById("row-" + i + "-col-" + j);
+       var currentBlock = draw(i, j);
        if(currentBlock.hasAttribute("style")) {
-         document.getElementById("row-" + (i + 1) + "-col-" + j).style.backgroundColor = currentBlock.style.backgroundColor;
-         if((i - 1) > 0)
+         draw(i + 1, j).style.backgroundColor = currentBlock.style.backgroundColor;
+         if((i - 1) > 0) {
            currentBlock.style = null;
+         }
        }
      }
    }
@@ -182,17 +176,14 @@ function clearFullRow() {
 
 function square(nextBlock, secondNextBlock) {
 
-  if(nextBlock === false && secondNextBlock) {
-    draw(row, col+1).style.backgroundColor = currentBlock.style.backgroundColor;
-    draw(row, col).style.backgroundColor = currentBlock.style.backgroundColor;
+  // if(nextBlock === false && secondNextBlock) {
+  //   draw(row, col+1).style.backgroundColor = currentBlock.style.backgroundColor;
+  //   draw(row, col).style.backgroundColor = currentBlock.style.backgroundColor;
+  //
+  // } else {
 
-  } else {
-    var currentBlock = draw(row, col);
-    currentBlock.style.backgroundColor = color[colorIndex];
-    draw(row, col+1).style.backgroundColor = currentBlock.style.backgroundColor;
-    draw(row+1, col).style.backgroundColor = currentBlock.style.backgroundColor;
-    draw(row+1, col+1).style.backgroundColor = currentBlock.style.backgroundColor;
-
-
-  }
+    // draw(row, col+1).style.backgroundColor = currentBlock.style.backgroundColor;
+    // draw(row+1, col).style.backgroundColor = currentBlock.style.backgroundColor;
+    // draw(row+1, col+1).style.backgroundColor = currentBlock.style.backgroundColor;
+  // }
 }
